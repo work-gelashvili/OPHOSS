843b2797240cb9cb7eba8e12b5ef086c5c1a6155
Davit Gelashvili <davit@gelashvili.info>
2024-03-11 21:54:34 +0400

    add

--- .next/server/app/page.js
+++ .next/server/app/page.js
@@ -1273,7 +1273,7 @@ const __default__ = proxy.default;
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = __webpack_require__.X(0, [878,402], () => (__webpack_exec__(1995)));
+var __webpack_exports__ = __webpack_require__.X(0, [878,572], () => (__webpack_exec__(1995)));
 module.exports = __webpack_exports__;
 
 })();
